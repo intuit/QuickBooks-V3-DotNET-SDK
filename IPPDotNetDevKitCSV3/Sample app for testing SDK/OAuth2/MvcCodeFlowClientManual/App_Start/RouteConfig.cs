67e6f882ac998fa3f705f9b879a2fbb619416047
Nikita Bhambhani <nikita_bhambhani@intuit.com>
2018-07-02 17:19:32 -0700

    Add OAuth2 MVC sample

---
+++ IPPDotNetDevKitCSV3/Sample app for testing SDK/OAuth2/MvcCodeFlowClientManual/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcCodeFlowClientManual
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "App", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                "ErrorHandler",
+                "App/Error/{errMsg}",
+                new { controller = "App", action = "Error", errMsg = UrlParameter.Optional }
+            );
+        }
+    }
+}
