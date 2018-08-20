398f8b631bbd971965975af22a60200307c2673c
nimish84 <nimisha.shrivastava84@gmail.com>
2018-08-19 19:25:48 -0700

    Updated License

--- IPPDotNetDevKitCSV3/Code/Intuit.Ipp.OAuth2PlatformClient/Jwk/JwkExtensions.cs
+++ IPPDotNetDevKitCSV3/Code/Intuit.Ipp.OAuth2PlatformClient/Jwk/JwkExtensions.cs
@@ -1,5 +1,7 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+﻿
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
 // Modified for Intuit's Oauth2 implementation
 
 
