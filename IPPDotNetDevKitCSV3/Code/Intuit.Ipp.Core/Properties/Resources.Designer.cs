f0d8511fe3f282b074636bc0aea6bd632817f664
nimish84 <nimisha.shrivastava84@gmail.com>
2018-11-20 01:21:50 -0800

    5.9.0

--- IPPDotNetDevKitCSV3/Code/Intuit.Ipp.Core/Properties/Resources.Designer.cs
+++ IPPDotNetDevKitCSV3/Code/Intuit.Ipp.Core/Properties/Resources.Designer.cs
@@ -97,7 +97,7 @@ namespace Intuit.Ipp.Core.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 33.
+        ///   Looks up a localized string similar to 34.
         /// </summary>
         internal static string DefaultMinorVersionValue {
             get {
