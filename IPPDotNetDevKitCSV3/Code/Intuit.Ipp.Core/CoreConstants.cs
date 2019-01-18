75a42a74e7055c1483988902eeb4d8b56b76ffc6
nimish84 <nimisha.shrivastava84@gmail.com>
2019-01-18 14:52:28 -0800

    Added support for TaxClassification and minoversion=35

--- IPPDotNetDevKitCSV3/Code/Intuit.Ipp.Core/CoreConstants.cs
+++ IPPDotNetDevKitCSV3/Code/Intuit.Ipp.Core/CoreConstants.cs
@@ -240,7 +240,7 @@ namespace Intuit.Ipp.Core
         /// <summary>
         /// The Request source header value.
         /// </summary>
-        public const string REQUESTSOURCEHEADER = "V3DotNetSDK5.9.1.0";
+        public const string REQUESTSOURCEHEADER = "V3DotNetSDK6.0.0.0.0";
 
         /// <summary>
         /// multipart/form-data format
