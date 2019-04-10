5592aa1d7b82704be1a81c77237d05fecf9b58a6
nimish84 <nimisha.shrivastava84@gmail.com>
2019-04-10 14:08:50 -0700

    Updated license on the class

--- IPPDotNetDevKitCSV3/Code/Intuit.Ipp.Utility/Common/CoreConstants.cs
+++ IPPDotNetDevKitCSV3/Code/Intuit.Ipp.Utility/Common/CoreConstants.cs
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-// <summary>This file contains SdkException.</summary>
+// 
 // <summary>This file contains Constants.</summary>
 ////*********************************************************
 
