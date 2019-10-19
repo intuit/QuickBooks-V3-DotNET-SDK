e956ac0f274f87007b53ac5655f1c6e567bdc993
nimish84 <nimisha.shrivastava84@gmail.com>
2019-10-19 03:32:31 -0700

    removing junk code

--- IPPDotNetDevKitCSV3/Sample app for testing SDK/OAuth2/MvcCodeFlowClientManual/Scripts/_references.js
+++
@@ -1,7 +0,0 @@
-﻿/// <reference path="jquery-3.3.1.js" />
-/// <autosync enabled="true" />
-/// <reference path="respond.js" />
-/// <reference path="respond.matchmedia.addlistener.js" />
-/// <reference path="npm.js" />
-/// <reference path="bootstrap.min.js" />
-/// <reference path="modernizr-2.8.3.js" />
